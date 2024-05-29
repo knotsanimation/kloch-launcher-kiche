4694a006fb0f337b54d3b8f4389db397352031c9
Liam Collod <lcollod@gmail.com>
2024-05-29 19:02:45 +0200

    refacto(download): use pythonning latest version
    moved some function to pythonning

--- kloch_kiche/_download.py
+++ kloch_kiche/_download.py
@@ -1,71 +1,15 @@
-import contextlib
 import hashlib
 import logging
 import tarfile
-import time
 import urllib.request
 from pathlib import Path
-from typing import Callable
 
 from ._github_dl import get_system_python_release_url
 from pythonning.web import download_file
-from pythonning.progress import ProgressBar
+from pythonning.progress import catch_download_progress
+from pythonning.benchmark import timeit
 
 LOGGER = logging.getLogger(__name__)
-
-
-class DownloadProgressBar(ProgressBar):
-    """
-    A progress bar for download, expressed in MB units.
-    """
-
-    byte_to_MB = 9.5367e-7
-
-    def __init__(self):
-        super().__init__(
-            prefix="downloading",
-            suffix="[{bar_index:<2.1f}MB/{bar_max:.1f}MB] elapsed {elapsed_time:.2f}s",
-        )
-
-    def show_progress(self, block_number, block_size, total_size):
-        """
-        To be used as callback during a download operation.
-
-        Args:
-            block_number: current block being downloaded, variable over time.
-            block_size: size of each download block, static over time.
-            total_size:
-                total size of all the block to download, static over time.
-                might not be provided which correspond to a value < 1.
-        """
-        if total_size < 1:
-            self.update()
-        else:
-            downloaded = block_number * block_size
-            self.set_progress(
-                total_progress=downloaded * self.byte_to_MB,
-                new_maximum=total_size * self.byte_to_MB,
-            )
-
-
-@contextlib.contextmanager
-def catch_progress():
-    progress_bar = DownloadProgressBar()
-    progress_bar.start()
-    try:
-        yield progress_bar
-    finally:
-        progress_bar.end()
-
-
-@contextlib.contextmanager
-def timeit(message: str, stream: Callable[[str], None]):
-    start = time.time()
-    try:
-        yield
-    finally:
-        end = time.time()
-        stream(f"{message}{end - start:.2f}s")
 
 
 def _read_shad256(sha256_url: str) -> str:
@@ -93,7 +37,7 @@ def download_python(python_version: str, target_dir: Path) -> Path:
     target_file = target_dir / release_url.basename
     LOGGER.info(f"downloading '{release_url.url}' to '{target_file}'")
     with timeit("download_file took ", LOGGER.debug):
-        with catch_progress() as progress_bar:
+        with catch_download_progress() as progress_bar:
             download_file(
                 url=release_url.url,
                 target_file=target_file,
@@ -109,7 +53,7 @@ def download_python(python_version: str, target_dir: Path) -> Path:
             f"Failed to validate checksum for downloaded release '{release_url}'"
         )
 
-    LOGGER.debug(f"extracting '{target_file}'")
+    LOGGER.info(f"extracting archive '{target_file}'")
     with timeit("tarfile.open took ", LOGGER.debug):
         with tarfile.open(target_file, mode="r:gz") as tar:
             tar.extractall(target_dir)
